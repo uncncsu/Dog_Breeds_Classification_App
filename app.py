826de192e4aa7b1d33fdb08002216b5d5b00ce76
Meg Sharma <megsharma@Megs-MacBook-Air-2.local>
2019-10-29 18:45:50 -0400

    app.py finished

--- app.py
+++ app.py
@@ -10,21 +10,18 @@ import numpy as np
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.preprocessing import image
-from keras.preprocessing import image
-import numpy as np
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-from keras.applications.xception import Xception
-from keras.applications.xception import preprocess_input as preprocess_input_xception, decode_predictions as decode_predictions_xception
 from keras.layers import GlobalAveragePooling2D
 from keras.layers import Dropout, Dense
 from keras.models import Sequential
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint 
+
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
 
 #SQLALCHEMY
 
@@ -43,7 +40,28 @@ dog_names = ['Affenpinscher', 'Afghan_hound', 'Airedale_terrier',
 'Anatolian_shepherd_dog', 'Australian_cattle_dog', 
 'Australian_shepherd', 'Australian_terrier', 'Basenji', 'Basset_hound', 
 'Beagle', 'Bearded_collie', 'Beauceron', 'Bedlington_terrier', 
-'Belgian_malinois', 'Belgian_sheepdog', 'Belgian_tervuren', 'Bernese_mountain_dog', 'Bichon_frise', 'Black_and_tan_coonhound', 'Black_russian_terrier', 'Bloodhound', 'Bluetick_coonhound', 'Border_collie', 'Border_terrier', 'Borzoi', 'Boston_terrier', 'Bouvier_des_flandres', 'Boxer', 'Boykin_spaniel', 'Briard', 'Brittany', 'Brussels_griffon', 'Bull_terrier', 'Bulldog', 'Bullmastiff', 'Cairn_terrier', 'Canaan_dog', 'Cane_corso', 'Cardigan_welsh_corgi', 'Cavalier_king_charles_spaniel', 'Chesapeake_bay_retriever', 'Chihuahua', 'Chinese_crested', 'Chinese_shar-pei', 'Chow_chow', 'Clumber_spaniel', 'Cocker_spaniel', 'Collie', 'Curly-coated_retriever', 'Dachshund', 'Dalmatian', 'Dandie_dinmont_terrier', 'Doberman_pinscher', 'Dogue_de_bordeaux', 'English_cocker_spaniel', 'English_setter', 'English_springer_spaniel', 'English_toy_spaniel', 'Entlebucher_mountain_dog', 'Field_spaniel', 'Finnish_spitz', 'Flat-coated_retriever', 'French_bulldog', 'German_pinscher', 'German_shepherd_dog', 'German_shorthaired_pointer', 'German_wirehaired_pointer', 'Giant_schnauzer', 'Glen_of_imaal_terrier', 'Golden_retriever', 'Gordon_setter', 'Great_dane', 'Great_pyrenees', 'Greater_swiss_mountain_dog', 'Greyhound', 'Havanese', 'Ibizan_hound', 'Icelandic_sheepdog', 'Irish_red_and_white_setter', 'Irish_setter', 'Irish_terrier', 'Irish_water_spaniel', 'Irish_wolfhound', 'Italian_greyhound', 'Japanese_chin', 'Keeshond', 'Kerry_blue_terrier', 'Komondor', 'Kuvasz', 'Labrador_retriever', 'Lakeland_terrier', 'Leonberger', 'Lhasa_apso', 'Lowchen', 'Maltese', 'Manchester_terrier', 'Mastiff', 'Miniature_schnauzer', 'Neapolitan_mastiff', 'Newfoundland', 'Norfolk_terrier', 'Norwegian_buhund', 'Norwegian_elkhound', 'Norwegian_lundehund', 'Norwich_terrier', 'Nova_scotia_duck_tolling_retriever', 'Old_english_sheepdog', 'Otterhound', 'Papillon', 'Parson_russell_terrier', 'Pekingese', 'Pembroke_welsh_corgi', 'Petit_basset_griffon_vendeen', 'Pharaoh_hound', 'Plott', 'Pointer', 'Pomeranian', 'Poodle', 'Portuguese_water_dog', 'Saint_bernard', 'Silky_terrier', 'Smooth_fox_terrier', 'Tibetan_mastiff', 'Welsh_springer_spaniel', 'Wirehaired_pointing_griffon', 'Xoloitzcuintli', 'Yorkshire_terrier']
+'Belgian_malinois', 'Belgian_sheepdog', 'Belgian_tervuren', 'Bernese_mountain_dog', 
+'Bichon_frise', 'Black_and_tan_coonhound', 'Black_russian_terrier', 'Bloodhound', 
+'Bluetick_coonhound', 'Border_collie', 'Border_terrier', 'Borzoi', 'Boston_terrier', 'Bouvier_des_flandres', 
+'Boxer', 'Boykin_spaniel', 'Briard', 'Brittany', 'Brussels_griffon', 'Bull_terrier', 'Bulldog', 'Bullmastiff', 
+'Cairn_terrier', 'Canaan_dog', 'Cane_corso', 'Cardigan_welsh_corgi', 'Cavalier_king_charles_spaniel', 
+'Chesapeake_bay_retriever', 'Chihuahua', 'Chinese_crested', 'Chinese_shar-pei', 'Chow_chow', 
+'Clumber_spaniel', 'Cocker_spaniel', 'Collie', 'Curly-coated_retriever', 'Dachshund', 'Dalmatian', 
+'Dandie_dinmont_terrier', 'Doberman_pinscher', 'Dogue_de_bordeaux', 'English_cocker_spaniel', 
+'English_setter', 'English_springer_spaniel', 'English_toy_spaniel', 'Entlebucher_mountain_dog', 
+'Field_spaniel', 'Finnish_spitz', 'Flat-coated_retriever', 'French_bulldog', 'German_pinscher', 
+'German_shepherd_dog', 'German_shorthaired_pointer', 'German_wirehaired_pointer', 
+'Giant_schnauzer', 'Glen_of_imaal_terrier', 'Golden_retriever', 'Gordon_setter', 'Great_dane', 
+'Great_pyrenees', 'Greater_swiss_mountain_dog', 'Greyhound', 'Havanese', 'Ibizan_hound', 'Icelandic_sheepdog', 
+'Irish_red_and_white_setter', 'Irish_setter', 'Irish_terrier', 'Irish_water_spaniel', 'Irish_wolfhound', 
+'Italian_greyhound', 'Japanese_chin', 'Keeshond', 'Kerry_blue_terrier', 'Komondor', 'Kuvasz', 'Labrador_retriever',
+'Lakeland_terrier', 'Leonberger', 'Lhasa_apso', 'Lowchen', 'Maltese', 'Manchester_terrier', 'Mastiff', 
+'Miniature_schnauzer', 'Neapolitan_mastiff', 'Newfoundland', 'Norfolk_terrier', 'Norwegian_buhund', 
+'Norwegian_elkhound', 'Norwegian_lundehund', 'Norwich_terrier', 'Nova_scotia_duck_tolling_retriever', 
+'Old_english_sheepdog', 'Otterhound', 'Papillon', 'Parson_russell_terrier', 'Pekingese', 'Pembroke_welsh_corgi', 
+'Petit_basset_griffon_vendeen', 'Pharaoh_hound', 'Plott', 'Pointer', 'Pomeranian', 'Poodle', 'Portuguese_water_dog',
+'Saint_bernard', 'Silky_terrier', 'Smooth_fox_terrier', 'Tibetan_mastiff', 'Welsh_springer_spaniel',
+'Wirehaired_pointing_griffon', 'Xoloitzcuintli', 'Yorkshire_terrier']
 
 # Define a flask app
 app = Flask(__name__)
@@ -55,51 +73,31 @@ db = SQLAlchemy(app)
 Base = automap_base()
 # reflect the tables
 Base.prepare(db.engine, reflect=True, schema="public")
-
 characteristic = Base.classes.breed_characterz
 
 
-# Model saved with Keras model.save()
+# Pull in saved model from models file
 MODEL_PATH = 'models/dogClassification.h5'
 
-# # Load your trained model
-model = load_model(MODEL_PATH)
-# model.Xception_predict_breed() 
-# ##model.dog_breed_algorithm()      
+# Load our trained model
+model = load_model(MODEL_PATH)     
 print('Model loaded. Start serving...')    
-# # # ##model._make_predict_function()          # Necessary
 
-# You can also use pretrained model from Keras
-# # Check https://keras.io/applications/
-# from keras.applications.resnet50 import ResNet50
-# model = Xception(include_top=True, weights='imagenet', pooling="avg")
-# model = ResNet50(weights='imagenet')
-# print('Model loaded. Check http://127.0.0.1:5000/')
-
+# Prepares image
 def extract_Xception(tensor):
-    from keras.applications.xception import Xception, preprocess_input
-    return Xception(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+   from keras.applications.xception import Xception, preprocess_input
+   return Xception(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
 
 
 def model_predict(img_path, model):
-    dog_names = ['Affenpinscher', 'Afghan_hound', 'Airedale_terrier', 
-'Akita', 'Alaskan_malamute', 'American_eskimo_dog', 'American_foxhound', 
-'American_staffordshire_terrier', 'American_water_spaniel', 
-'Anatolian_shepherd_dog', 'Australian_cattle_dog', 
-'Australian_shepherd', 'Australian_terrier', 'Basenji', 'Basset_hound', 
-'Beagle', 'Bearded_collie', 'Beauceron', 'Bedlington_terrier', 
-'Belgian_malinois', 'Belgian_sheepdog', 'Belgian_tervuren', 'Bernese_mountain_dog', 'Bichon_frise', 'Black_and_tan_coonhound', 'Black_russian_terrier', 'Bloodhound', 'Bluetick_coonhound', 'Border_collie', 'Border_terrier', 'Borzoi', 'Boston_terrier', 'Bouvier_des_flandres', 'Boxer', 'Boykin_spaniel', 'Briard', 'Brittany', 'Brussels_griffon', 'Bull_terrier', 'Bulldog', 'Bullmastiff', 'Cairn_terrier', 'Canaan_dog', 'Cane_corso', 'Cardigan_welsh_corgi', 'Cavalier_king_charles_spaniel', 'Chesapeake_bay_retriever', 'Chihuahua', 'Chinese_crested', 'Chinese_shar-pei', 'Chow_chow', 'Clumber_spaniel', 'Cocker_spaniel', 'Collie', 'Curly-coated_retriever', 'Dachshund', 'Dalmatian', 'Dandie_dinmont_terrier', 'Doberman_pinscher', 'Dogue_de_bordeaux', 'English_cocker_spaniel', 'English_setter', 'English_springer_spaniel', 'English_toy_spaniel', 'Entlebucher_mountain_dog', 'Field_spaniel', 'Finnish_spitz', 'Flat-coated_retriever', 'French_bulldog', 'German_pinscher', 'German_shepherd_dog', 'German_shorthaired_pointer', 'German_wirehaired_pointer', 'Giant_schnauzer', 'Glen_of_imaal_terrier', 'Golden_retriever', 'Gordon_setter', 'Great_dane', 'Great_pyrenees', 'Greater_swiss_mountain_dog', 'Greyhound', 'Havanese', 'Ibizan_hound', 'Icelandic_sheepdog', 'Irish_red_and_white_setter', 'Irish_setter', 'Irish_terrier', 'Irish_water_spaniel', 'Irish_wolfhound', 'Italian_greyhound', 'Japanese_chin', 'Keeshond', 'Kerry_blue_terrier', 'Komondor', 'Kuvasz', 'Labrador_retriever', 'Lakeland_terrier', 'Leonberger', 'Lhasa_apso', 'Lowchen', 'Maltese', 'Manchester_terrier', 'Mastiff', 'Miniature_schnauzer', 'Neapolitan_mastiff', 'Newfoundland', 'Norfolk_terrier', 'Norwegian_buhund', 'Norwegian_elkhound', 'Norwegian_lundehund', 'Norwich_terrier', 'Nova_scotia_duck_tolling_retriever', 'Old_english_sheepdog', 'Otterhound', 'Papillon', 'Parson_russell_terrier', 'Pekingese', 'Pembroke_welsh_corgi', 'Petit_basset_griffon_vendeen', 'Pharaoh_hound', 'Plott', 'Pointer', 'Pomeranian', 'Poodle', 'Portuguese_water_dog', 'Saint_bernard', 'Silky_terrier', 'Smooth_fox_terrier', 'Tibetan_mastiff', 'Welsh_springer_spaniel', 'Wirehaired_pointing_griffon', 'Xoloitzcuintli', 'Yorkshire_terrier']
-    # recreating the path_to_tensor function
-    # loads RGB image as PIL.Image.Image type
+    # loads image and process to expected format
     img = image.load_img(img_path, target_size=(224,224))
-    # convert PIL.Image.Image type to 3D tensor with shape (224, 224)
     x = image.img_to_array(img)
-    # convert 3D tensor to 4D tensor with shape (1,224, 224, 3) and return 4D tensor
     test_image = np.expand_dims(x, axis=0)
     test_image = extract_Xception(test_image)
-    # Xception_model = Sequential()
+    #Make prediction using model
     predicted_vector = model.predict(test_image)
-    print(predicted_vector)
+    #Determine highest probability of associated breed
     preds = dog_names[np.argmax(predicted_vector)]
     return preds
 
@@ -122,20 +120,15 @@ def upload():
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
-        # # Make prediction
+        # Obtain prediction
         preds = model_predict(file_path, model)
         breed = str(preds)
-        # # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        # result = str(pred_class[0][0][1])               # Convert to string
         return breed
     return None
 
+# Obtains data for particular dog breed
 @app.route("/predict/<breed>")
 def get_data_results(breed):
-    """Return the data for a given year
-    ."""
     sel = [
         characteristic.id,
         characteristic.BreedName,
@@ -176,7 +169,6 @@ def get_data_results(breed):
 
 
 if __name__ == '__main__':
-    # app.run(port=5002, debug=True)
 
     # Serve the app with gevent
     http_server = WSGIServer(('0.0.0.0', 4008), app)
